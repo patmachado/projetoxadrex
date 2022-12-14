cdebe894ea14d7b971f69402751a7408211f097d
patmachado <patrickmachado@hotmail.com>
2022-12-14 12:39:22 -0300

    Inserindo .gitignore e Classes

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xadrex_console.tabuleiro;
 
 namespace xadrex_console
 {
@@ -6,7 +7,11 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
+
         }
     }
 }
