2f8eec450315c1a2d66a9a41af0be32f948af626
patmachado <patrickmachado@hotmail.com>
2022-12-14 13:09:21 -0300

    Inclusao de Classes Tabuleiro

---
+++ xadrex-console/tabuleiro/Cor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrex_console.tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
